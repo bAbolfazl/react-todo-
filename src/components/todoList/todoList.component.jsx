860eed56e4abb537b0f545ee714386cdace33d0b
Abolfazl <a.baghshahi1327@gmail.com>
2020-12-06 22:26:02 +0330

    add readme

--- src/components/todoList/todoList.component.jsx
+++ src/components/todoList/todoList.component.jsx
@@ -7,11 +7,19 @@ const TodoList = ({ todoList, setTodoList }) => {
         setTodoList(filtered)
     }
 
+    const calcColor = (index) => {
+        // const i = String(index)
+        let color = `68, 205, 239, .${index}`
+        console.log(color)
+        return color
+    }
+
     return (
         <div className="w-50 bg-secondary mx-auto">
             {
                 todoList?.map((item, index) =>
-                    <div key={index} className="d-flex align-items-center justify-content-between my-2 px-3 ">
+                    <div key={index} className='d-flex align-items-center justify-content-between my-2 px-3'
+                        style={{ backgroundColor: `rgba(${calcColor(index)})` }}>
                         <span>{index + 1}</span>
                         <span>{item}</span>
                         <button onClick={handle_delete_click(item)} className="btn btn-danger">Delete</button>
